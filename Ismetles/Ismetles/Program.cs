9bead02a1ce79318813bf14f826d2a4587bc8cd3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-11 08:33:20 +0200

    Tört számok

--- Ismetles/Ismetles/Program.cs
+++ Ismetles/Ismetles/Program.cs
@@ -21,12 +21,33 @@ namespace Ismetles
             a = 119;
             a = -119;
 
-            Console.WriteLine($"Max:{Int32.MaxValue} Min{Int32.MinValue}");
+            Console.WriteLine($"Max:{Int32.MaxValue} Min:{Int32.MinValue}");
 
             uint aa = 35;
+
+            byte b = 122;
+
+            short bb = 120;
+
+            long c = 1245366662456;
+
+            //nem egész számok
+            //egyszeres pontosságú lebegőpontos szám
+            float t=23.012345678901234567890123456789f;
+
+            //kétszeres pontosságú
+            double tt = 23.012345678901234567890123456789;
+
+            //pénzügyi alkalmazásokhoz
+            decimal ttt = 23.012345678901234567890123456789m;
+
+            Console.WriteLine($"{t},{tt},{ttt}");
+
                       
 
             Console.WriteLine($"Max:{UInt32.MaxValue} Min{UInt32.MinValue}");
+
+
 
             Console.ReadKey();
         }

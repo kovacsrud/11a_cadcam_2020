da6f9a1be3faa95bc879d4d565f481ccfaa55937
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-25 08:31:03 +0200

    Update Program.cs

--- Szovegek/Szovegek/Program.cs
+++ Szovegek/Szovegek/Program.cs
@@ -17,7 +17,25 @@ namespace Szovegek
             Console.WriteLine(szoveg.ToUpper());
             Console.WriteLine(szoveg.ToLower());
 
+            if (szoveg.ToUpper()==masik.ToUpper())
+            {
+                Console.WriteLine("Megegyeznek");
+            } else
+            {
+                Console.WriteLine("Nem egyeznek meg");
+            }
 
+            //Adott szöveggek kezdődik-e?
+            Console.WriteLine(szoveg.StartsWith("Val"));
+            Console.WriteLine(szoveg.EndsWith("ami"));
+
+            //Tartalmaz e a változó egy szöveget?
+            Console.WriteLine(szoveg.Contains("ami"));
+
+            //Szövegrész kiemelése
+            string datum = "2020.09.19";
+            Console.WriteLine(datum.Substring(5,2));
+            Console.WriteLine(datum.Substring(0,4));
 
 
 

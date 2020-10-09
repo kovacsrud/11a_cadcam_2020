da32c647b56463dd51c848f8d2823d165703922e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-09 08:07:48 +0200

    Ember osztály

---
+++ Osztalyok/Osztalyok/Ember.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok
+{
+    public class Ember
+    {
+        //adattagok, adatmezők
+        public string nev;
+        public int magassag;
+        public int suly;
+        public int szulev;
+
+        //metódusok (függvények)
+        public void Koszon()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
---
+++ Osztalyok/Osztalyok/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ember ubul = new Ember();
+
+            Console.ReadKey();
+        }
+    }
+}

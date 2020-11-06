8915302c19b75811603ea9d4c2950d5753085767
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-06 08:25:33 +0100

    Osztály létrehozása

--- Idojaras/Idojaras/Program.cs
+++ Idojaras/Idojaras/Program.cs
@@ -10,6 +10,9 @@ namespace Idojaras
     {
         static void Main(string[] args)
         {
+            IdojarasAdat adat = new IdojarasAdat(2000, 12, 22, 11, 12.5, 13.5,14.5);
+
+            Console.ReadKey();
         }
     }
 }

128f87d7bfee8ef35ba1b8eebd678d0416a1f69f
szajberpiraty <szajberpiraty@gmail.com>
2020-11-20 09:04:48 +0100

    Hegysegek fájlbetöltés

---
+++ Hegyek/Hegyek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hegyek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hegycsucs> hegycsucsok = new List<Hegycsucs>();
+
+            try
+            {
+                var sorok = File.ReadAllLines("hegyekMo.txt",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(';');
+                    hegycsucsok.Add(
+                        new Hegycsucs
+                        {
+                            HegycsucsNeve = e[0],
+                            Hegyseg = e[1],
+                            Magassag = Convert.ToInt32(e[2])
+                        }
+                        );
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"{hegycsucsok.Count} hegy található.");
+        }
+    }
+}

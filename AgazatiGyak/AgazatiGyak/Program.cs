e66f3b257e6846da0154ee096c233b17fe7e755b
szajberpiraty <szajberpiraty@gmail.com>
2021-01-15 08:30:08 +0100

    Gyakorló

---
+++ AgazatiGyak/AgazatiGyak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgazatiGyak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adjon meg egy számot:");
+            var szam = Convert.ToInt32(Console.ReadLine());
+
+            if (szam>0)
+            {
+                Console.WriteLine("Ez a szám pozitív.");
+            } else if (szam<0)
+            {
+                Console.WriteLine("Ez a szám negatív.");
+            } else
+            {
+                Console.WriteLine("A szám nulla.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

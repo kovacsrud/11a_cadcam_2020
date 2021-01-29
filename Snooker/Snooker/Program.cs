2ce27f78a9d01dddc4f303d88d0e3ba795e96be9
szajberpiraty <szajberpiraty@gmail.com>
2021-01-29 08:49:41 +0100

    Update Program.cs

--- Snooker/Snooker/Program.cs
+++ Snooker/Snooker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace Snooker
     {
         static void Main(string[] args)
         {
+            List<Versenyzo> versenyzok = new List<Versenyzo>();
+            try
+            {
+                var sorok = File.ReadAllLines("snooker.txt",Encoding.Default);
+
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    versenyzok.Add(new Versenyzo(sorok[i]));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine($"3.feladat:A világranglistán {versenyzok.Count} versenyző szerepel.");
+
+            var atlagkereset = versenyzok.Average(x => x.Nyeremeny);
+            
+            Console.WriteLine($"4.feladat: A versenyzők átlagosan {atlagkereset:0.00} fontot kerestek.");
 
 
             Console.ReadKey();

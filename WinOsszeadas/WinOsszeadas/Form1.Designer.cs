8efa3347b1f8ab1fce47932de76ee8c691c6fa68
szajberpiraty <szajberpiraty@gmail.com>
2021-02-26 08:29:44 +0100

    Start

---
+++ WinOsszeadas/WinOsszeadas/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace WinOsszeadas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelEredmeny = new System.Windows.Forms.Label();
+            this.buttonSzamolas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(43, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A változó:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(43, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B változó:";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxA.Location = new System.Drawing.Point(183, 85);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(155, 43);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxB.Location = new System.Drawing.Point(183, 149);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(155, 43);
+            this.textBoxB.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(43, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 37);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Eredmény:";
+            // 
+            // labelEredmeny
+            // 
+            this.labelEredmeny.AutoSize = true;
+            this.labelEredmeny.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelEredmeny.Location = new System.Drawing.Point(183, 258);
+            this.labelEredmeny.Name = "labelEredmeny";
+            this.labelEredmeny.Size = new System.Drawing.Size(135, 37);
+            this.labelEredmeny.TabIndex = 5;
+            this.labelEredmeny.Text = "eredmeny";
+            // 
+            // buttonSzamolas
+            // 
+            this.buttonSzamolas.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonSzamolas.Location = new System.Drawing.Point(331, 338);
+            this.buttonSzamolas.Name = "buttonSzamolas";
+            this.buttonSzamolas.Size = new System.Drawing.Size(122, 57);
+            this.buttonSzamolas.TabIndex = 6;
+            this.buttonSzamolas.Text = "Számolj!";
+            this.buttonSzamolas.UseVisualStyleBackColor = true;
+            this.buttonSzamolas.Click += new System.EventHandler(this.buttonSzamolas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSzamolas);
+            this.Controls.Add(this.labelEredmeny);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelEredmeny;
+        private System.Windows.Forms.Button buttonSzamolas;
+    }
+}
+
---
+++ WinOsszeadas/WinOsszeadas/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinOsszeadas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSzamolas_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var a = Convert.ToDouble(textBoxA.Text);
+                var b = Convert.ToDouble(textBoxB.Text);
+                var eredmeny = a + b;
+                labelEredmeny.Text = Convert.ToString(eredmeny);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba, számot kell megadni", "Hiba!",MessageBoxButtons.OK,MessageBoxIcon.Error);                
+            }
+           
+        }
+    }
+}

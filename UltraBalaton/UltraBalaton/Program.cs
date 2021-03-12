5ec06b9a5cfcf8636160801ae7951a84d761197f
szajberpiraty <szajberpiraty@gmail.com>
2021-03-12 08:53:38 +0100

    Ultrabalaton

---
+++ UltraBalaton/UltraBalaton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraBalaton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new List<Versenyzo>();
+            try
+            {
+                var sorok = File.ReadAllLines("ub2017egyeni.txt", Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    versenyzok.Add(new Versenyzo(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine($"3.feladat Egyéni indulók:{versenyzok.Count} fő.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UltraBalaton/UltraBalaton/Versenyzo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraBalaton
+{
+    public class Versenyzo
+    {
+        public string VersenyzoNev { get; set; }
+        public int Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Versenyido { get; set; }
+        public int Tavszazalek { get; set; }
+
+        public Versenyzo(string sor)
+        {
+            var e = sor.Split(';');
+            VersenyzoNev = e[0];
+            Rajtszam = Convert.ToInt32(e[1]);
+            Kategoria = e[2];
+            Versenyido = e[3];
+            Tavszazalek = Convert.ToInt32(e[4]);
+
+        }
+
+        public double IdoOraban()
+        {
+            var e = Versenyido.Split(':');
+            int ora = Convert.ToInt32(e[0]);
+            int perc = Convert.ToInt32(e[1]);
+            int masodperc = Convert.ToInt32(e[2]);
+
+            var osszora = ((ora * 3600) + (perc * 60) + masodperc)/3600;
+            return osszora;
+        }
+    }
+}

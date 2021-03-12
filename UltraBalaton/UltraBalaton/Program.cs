358800a39e782c00ff9a9bc17a53987523617039
szajberpiraty <szajberpiraty@gmail.com>
2021-03-12 09:08:52 +0100

    Update Program.cs

--- UltraBalaton/UltraBalaton/Program.cs
+++ UltraBalaton/UltraBalaton/Program.cs
@@ -26,6 +26,31 @@ namespace UltraBalaton
             }
 
             Console.WriteLine($"3.feladat Egyéni indulók:{versenyzok.Count} fő.");
+            var noi = versenyzok.FindAll(x=>x.Kategoria== "Noi" && x.Tavszazalek==100).Count;
+
+            Console.WriteLine($"4.feladat Célba érkező női sportolók:{noi} fő.");
+
+            Console.Write("Adja meg egy versenyző nevét:");
+            var beNev = Console.ReadLine();
+
+            var sportolo = versenyzok.Find(x=>x.VersenyzoNev==beNev);
+
+            if (sportolo!=null)
+            {
+                Console.WriteLine("Indult egyéniben a versenyző? Igen");
+                if (sportolo.Tavszazalek==100)
+                {
+                    Console.WriteLine("Teljesítette a teljes távot:Igen");
+                } else
+                {
+                    Console.WriteLine("Teljesítette a teljes távot:Nem");
+                }
+                
+            } else
+            {
+                Console.WriteLine("Indult egyéniben a versenyző? Nem");
+            }
+
 
             Console.ReadKey();
         }

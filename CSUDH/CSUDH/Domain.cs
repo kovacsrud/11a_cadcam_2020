624a6948624d5cbe8a0a7477cfc3a23c3de33697
szajberpiraty <szajberpiraty@gmail.com>
2021-03-26 08:24:52 +0100

    csudh

---
+++ CSUDH/CSUDH/Domain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSUDH
+{
+    public class Domain
+    {
+        public string DomainName { get; set; }
+        public string IPAddress { get; set; }
+
+        public Domain(string sor)
+        {
+            var e = sor.Split(';');
+            DomainName = e[0];
+            IPAddress = e[1];
+        }
+
+    }
+}
---
+++ CSUDH/CSUDH/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSUDH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Domain> domains = new List<Domain>();
+            try
+            {
+                var sorok = File.ReadAllLines("csudh.txt",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    domains.Add(new Domain(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

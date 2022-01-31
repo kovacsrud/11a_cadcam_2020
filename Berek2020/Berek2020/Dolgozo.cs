6c47c547b86500815888dc03c4bcd7ae71ef66cf
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2022-01-31 09:15:38 +0100

    Berek2020

---
+++ Berek2020/Berek2020/Dolgozo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2020
+{
+    public class Dolgozo
+    {
+        public string Nev { get; set; }
+        public string Neme { get; set; }
+        public string Reszleg { get; set; }
+        public int Belepes { get; set; }
+        public int Ber { get; set; }
+
+        public Dolgozo(string sor)
+        {
+            var adatok = sor.Split(';');
+            Nev = adatok[0];
+            Neme = adatok[1];
+            Reszleg = adatok[2];
+            Belepes = Convert.ToInt32(adatok[3]);
+            Ber = Convert.ToInt32(adatok[4]);
+        }
+    }
+}
---
+++ Berek2020/Berek2020/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dolgozo> dolgozok = new List<Dolgozo>();
+            try
+            {
+                var sorok = File.ReadAllLines("berek2020.txt", Encoding.UTF8);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    dolgozok.Add(new Dolgozo(sorok[i]));
+
+                }
+                Console.WriteLine("Kész");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"3.feladat: Dolgozók száma:{dolgozok.Count} fő");
+
+
+            var atlagber = dolgozok.Average(x=>x.Ber);
+            Console.WriteLine($"4.feladat:Bérek átlaga:{atlagber/1000:0.0} eFt");
+
+            var reszlegNev = "";
+            Console.Write("5.feladat: Kérem egy részleg nevét:");
+            reszlegNev = Console.ReadLine();
+
+            var reszlegAdatok = dolgozok.FindAll(x => x.Reszleg.ToLower() == reszlegNev.ToLower());
+
+            if (reszlegAdatok.Count > 0)
+            {
+                var legtobbetKereso = reszlegAdatok.Find(x=>x.Ber==reszlegAdatok.Max(y=>y.Ber));
+                Console.WriteLine($@"6.feladat: A legtöbbet kereső a megadott részlegen:
+                Név:{legtobbetKereso.Nev}
+                Neme:{legtobbetKereso.Neme}
+                Belépés éve:{legtobbetKereso.Belepes}
+                Bér:{legtobbetKereso.Ber} Forint");
+
+            } else
+            {
+                Console.WriteLine("Nincs ilyen részleg!");
+            }
+
+            var statisztika = dolgozok.ToLookup(x=>x.Reszleg).OrderBy(x=>x.Key);
+
+            foreach (var i in statisztika)
+            {
+                Console.WriteLine($"{i.Key},{i.Count()} fő");
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
